02a3ae2d3563fafcb04b699f1c8028625de74c33
wubx <wubingxi@gmail.com>
2022-02-28 09:06:04 +0800

    适应新版本

--- bench/ontime/load_ontime.sh
+++ bench/ontime/load_ontime.sh
@@ -1,15 +1,6 @@
-cat create_ontime.sql |mysql -h127.0.0.1 -P3307 -uroot 
-if [ $? -eq  0 ];
-then 
-    echo "Ontime table create success"
-else
-    echo "Ontime table create was wrong!!!"
-    exit 1
-fi
+echo "unzip ontime ,input your ontime zip dir: ./load_ontime.sh zip_dir"
 
-echo "unzip ontime ,input your ontime zip dir:"
-
-ls $1/*.zip |xargs -I{} bash -c "unzip  {} -d ./dataset"
+ls $1/*.zip |xargs -I{} bash -c "echo {}; unzip -q {} '*.csv' -d ./dataset"
 
 if [ $? -eq  0 ];
 then 
@@ -19,4 +10,14 @@ else
     exit 1
 fi
 
-time ls ./dataset/*.csv|xargs -P 8 -I{} curl -H "insert_sql:insert into ontime format CSV" -H "csv_header:1" -F "upload=@{}" -XPUT http://localhost:8000/v1/streaming_load
+cat create_ontime.sql |mysql -h127.0.0.1 -P3307 -uroot 
+if [ $? -eq  0 ];
+then 
+    echo "Ontime table create success"
+else
+    echo "Ontime table create was wrong!!!"
+    exit 1
+fi
+
+
+time ls ./dataset/*.csv|xargs -P 8 -I{} curl -H "insert_sql:insert into ontime format CSV" -H "skip_header:1" -F "upload=@{}" -XPUT http://localhost:8000/v1/streaming_load
